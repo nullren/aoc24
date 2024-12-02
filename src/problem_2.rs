435128e43fa4919249378a9adf359c2268f0a551
Renning Bruns <ren@renmail.net>
2024-12-01 21:22:02 -0800

    added problem_2

--- src/problem_2.rs
+++ src/problem_2.rs
@@ -1,3 +1,7 @@
+pub fn part_1(input: &str) -> i32 {
+    todo!()
+}
+
 pub fn part_2(input: &str) -> i32 {
     todo!()
 }
@@ -7,13 +11,13 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_problem_2_part_1() {
+    fn test_part_1() {
         let input = "7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9";
-        assert_eq!(part_2(input), 2);
+        assert_eq!(part_1(input), 2);
     }
 }
