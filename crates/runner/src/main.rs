218427911ff4224f6f9842860c062127a4f0f274
Renning Bruns <ren@renmail.net>
2024-12-03 19:49:30 -0800

    moved shit to crates

---
+++ crates/runner/src/main.rs
@@ -0,0 +1,7 @@
+use problem_2::{part_1, part_2};
+
+fn main() {
+    let input = include_str!("../inputs/2.txt");
+    println!("part1: {}", part_1(input));
+    println!("part2: {}", part_2(input));
+}
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-mod problem_1;
-mod problem_2;
-
-fn main() {
-    let input = include_str!("../inputs/2.txt");
-    println!("part1: {}", problem_2::part_1(input));
-    println!("part2: {}", problem_2::part_2(input));
-}
