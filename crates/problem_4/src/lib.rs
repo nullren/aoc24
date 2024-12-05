f3383bbf670f2bca89adf727e8c7021c7c3b311c
Renning Bruns <ren@renmail.net>
2024-12-04 19:56:59 -0800

    problem 4 part 1 done

--- crates/problem_4/src/lib.rs
+++ crates/problem_4/src/lib.rs
@@ -1,5 +1,129 @@
-pub fn part_1(_input: &str) -> Option<i32> {
-    None
+pub fn part_1(input: &str) -> Option<i32> {
+    let grid = Grid::new(input);
+
+    let mut xmas_count = 0;
+    for (y, row) in grid.enumerate() {
+        for (x, col) in row.iter().enumerate() {
+            if *col == 'X' {
+                xmas_count += grid.count_xmases_at(x, y);
+            }
+        }
+    }
+    Some(xmas_count)
+}
+
+struct Grid {
+    grid: Vec<Vec<char>>,
+}
+
+impl Grid {
+    fn new(input: &str) -> Self {
+        let grid = input
+            .lines()
+            .map(|line| line.chars().collect::<Vec<char>>())
+            .collect::<Vec<Vec<char>>>();
+        Self { grid }
+    }
+
+    fn enumerate(&self) -> impl Iterator<Item = (usize, &Vec<char>)> {
+        self.grid.iter().enumerate()
+    }
+
+    fn xmas_up(&self, x: usize, y: usize) -> bool {
+        if y < 3 {
+            return false;
+        }
+        self.grid[y][x] == 'X'
+            && self.grid[y - 1][x] == 'M'
+            && self.grid[y - 2][x] == 'A'
+            && self.grid[y - 3][x] == 'S'
+    }
+
+    fn xmas_down(&self, x: usize, y: usize) -> bool {
+        if y > self.grid.len() - 4 {
+            return false;
+        }
+        self.grid[y][x] == 'X'
+            && self.grid[y + 1][x] == 'M'
+            && self.grid[y + 2][x] == 'A'
+            && self.grid[y + 3][x] == 'S'
+    }
+
+    fn xmas_left(&self, x: usize, y: usize) -> bool {
+        if x < 3 {
+            return false;
+        }
+        self.grid[y][x] == 'X'
+            && self.grid[y][x - 1] == 'M'
+            && self.grid[y][x - 2] == 'A'
+            && self.grid[y][x - 3] == 'S'
+    }
+
+    fn xmas_right(&self, x: usize, y: usize) -> bool {
+        if x > self.grid[0].len() - 4 {
+            return false;
+        }
+        self.grid[y][x] == 'X'
+            && self.grid[y][x + 1] == 'M'
+            && self.grid[y][x + 2] == 'A'
+            && self.grid[y][x + 3] == 'S'
+    }
+
+    fn xmas_up_left(&self, x: usize, y: usize) -> bool {
+        if y < 3 || x < 3 {
+            return false;
+        }
+        self.grid[y][x] == 'X'
+            && self.grid[y - 1][x - 1] == 'M'
+            && self.grid[y - 2][x - 2] == 'A'
+            && self.grid[y - 3][x - 3] == 'S'
+    }
+
+    fn xmas_up_right(&self, x: usize, y: usize) -> bool {
+        if y < 3 || x > self.grid[0].len() - 4 {
+            return false;
+        }
+        self.grid[y][x] == 'X'
+            && self.grid[y - 1][x + 1] == 'M'
+            && self.grid[y - 2][x + 2] == 'A'
+            && self.grid[y - 3][x + 3] == 'S'
+    }
+
+    fn xmas_down_left(&self, x: usize, y: usize) -> bool {
+        if y > self.grid.len() - 4 || x < 3 {
+            return false;
+        }
+        self.grid[y][x] == 'X'
+            && self.grid[y + 1][x - 1] == 'M'
+            && self.grid[y + 2][x - 2] == 'A'
+            && self.grid[y + 3][x - 3] == 'S'
+    }
+
+    fn xmas_down_right(&self, x: usize, y: usize) -> bool {
+        if y > self.grid.len() - 4 || x > self.grid[0].len() - 4 {
+            return false;
+        }
+        self.grid[y][x] == 'X'
+            && self.grid[y + 1][x + 1] == 'M'
+            && self.grid[y + 2][x + 2] == 'A'
+            && self.grid[y + 3][x + 3] == 'S'
+    }
+
+    fn count_xmases_at(&self, x: usize, y: usize) -> i32 {
+        [
+            self.xmas_up(x, y),
+            self.xmas_down(x, y),
+            self.xmas_left(x, y),
+            self.xmas_right(x, y),
+            self.xmas_up_left(x, y),
+            self.xmas_up_right(x, y),
+            self.xmas_down_left(x, y),
+            self.xmas_down_right(x, y),
+        ]
+        .iter()
+        .filter(|&&x| x)
+        .count() as i32
+    }
 }
 
 pub fn part_2(_input: &str) -> Option<i32> {
