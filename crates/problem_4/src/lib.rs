61f6d30cb3a96c07464667a44dda0cbf39a88ea3
Renning Bruns <ren@renmail.net>
2024-12-04 19:30:48 -0800

    add example for part 1

--- crates/problem_4/src/lib.rs
+++ crates/problem_4/src/lib.rs
@@ -10,12 +10,21 @@ pub fn part_2(_input: &str) -> Option<i32> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
 
     #[test]
     fn test_part_1() {
         let result = part_1(INPUT);
-        assert_eq!(result, None);
+        assert_eq!(result, Some(18));
     }
 
     #[test]
