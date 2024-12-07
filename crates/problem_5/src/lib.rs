9bd74045485ca991877f2c0a6ac5f8a62433d8a2
Renning Bruns <ren@renmail.net>
2024-12-07 13:27:50 -0800

    tests pass

--- crates/problem_5/src/lib.rs
+++ crates/problem_5/src/lib.rs
@@ -1,10 +1,20 @@
 use std::collections::{HashMap, HashSet};
 
-pub fn part_1(_input: &str) -> Option<i32> {
-    None
+type Rules = HashMap<String, HashSet<String>>;
+
+pub fn part_1(input: &str) -> Option<i32> {
+    let (part_1, part_2) = input.split_once("\n\n").unwrap();
+    let mut sum = 0;
+    let rules = rules(part_1);
+    for line in part_2.lines() {
+        if let Some(middle) = get_middle_if_correct(line, &rules) {
+            sum += middle;
+        }
+    }
+    Some(sum)
 }
 
-fn rules(input: &str) -> HashMap<String, HashSet<String>> {
+fn rules(input: &str) -> Rules {
     let mut rules = HashMap::new();
     for line in input.lines() {
         let (key, value) = line.split_once("|").unwrap();
@@ -13,6 +23,40 @@ fn rules(input: &str) -> HashMap<String, HashSet<String>> {
         set.insert(value.to_string());
     }
     rules
+}
+
+fn pages(input: &str) -> Vec<i32> {
+    input
+        .split(",")
+        .map(|page| page.parse::<i32>().unwrap())
+        .collect()
+}
+
+fn middle_page(pages: Vec<i32>) -> i32 {
+    let middle = pages.len() / 2;
+    pages[middle]
+}
+
+fn correct_order(pages: Vec<i32>, rules: &Rules) -> bool {
+    let mut seen = HashSet::new();
+    let empty = HashSet::new();
+    for page in &pages {
+        let cannot_be = rules.get(&page.to_string()).unwrap_or(&empty);
+        if seen.intersection(cannot_be).count() > 0 {
+            return false;
+        }
+        seen.insert(page.to_string());
+    }
+    true
+}
+
+fn get_middle_if_correct(input: &str, rules: &Rules) -> Option<i32> {
+    let pages = pages(input);
+    if correct_order(pages.clone(), &rules) {
+        Some(middle_page(pages))
+    } else {
+        None
+    }
 }
 
 pub fn part_2(_input: &str) -> Option<i32> {
