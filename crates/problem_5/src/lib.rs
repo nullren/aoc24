1d3da068b52b5f496e35c4fc959965a5f8e15eeb
Renning Bruns <ren@renmail.net>
2024-12-07 13:04:05 -0800

    add example

--- crates/problem_5/src/lib.rs
+++ crates/problem_5/src/lib.rs
@@ -10,12 +10,39 @@ pub fn part_2(_input: &str) -> Option<i32> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
 
     #[test]
     fn test_part_1() {
         let result = part_1(INPUT);
-        assert_eq!(result, None);
+        assert_eq!(result, Some(143));
     }
 
     #[test]
