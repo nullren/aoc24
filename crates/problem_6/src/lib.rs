eeddf5cf3d428e213fdd29ead10ac73532c008d8
Renning Bruns <ren@renmail.net>
2024-12-08 12:06:45 -0800

    problem

--- crates/problem_6/src/lib.rs
+++ crates/problem_6/src/lib.rs
@@ -10,12 +10,21 @@ pub fn part_2(_input: &str) -> Option<i32> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
 
     #[test]
     fn test_part_1() {
         let result = part_1(INPUT);
-        assert_eq!(result, None);
+        assert_eq!(result, Some(41));
     }
 
     #[test]
