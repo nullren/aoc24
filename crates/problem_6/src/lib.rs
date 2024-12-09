5920f2e063fb76d3244593d82368ac790b8fa7f1
Renning Bruns <ren@renmail.net>
2024-12-08 20:38:29 -0800

    this does not work

--- crates/problem_6/src/lib.rs
+++ crates/problem_6/src/lib.rs
@@ -1,8 +1,9 @@
 use std::collections::HashSet;
 
 pub fn part_1(input: &str) -> Option<i32> {
-    let mut world = World::new(input);
-    Some(world.run() as i32)
+    let world = World::new(input);
+    let guard = world.find_guard();
+    Some(run(&world, guard) as i32)
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -11,13 +12,19 @@ struct Position {
     col: usize,
 }
 
+impl std::fmt::Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.row, self.col)
+    }
+}
+
 impl Position {
     fn new(row: usize, col: usize) -> Self {
         Self { row, col }
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Down,
@@ -50,7 +57,7 @@ impl Direction {
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct Guard {
     position: Position,
     direction: Direction,
@@ -66,7 +73,7 @@ impl Guard {
 
     /// Returns the position of the space the guard is looking at,
     /// otherwise None if the guard is looking at the edge of the world.
-    fn peek(&self, world: &World) -> Option<(char, Position)> {
+    fn peek(&self, grid: &[Vec<char>]) -> Option<(char, Position)> {
         let row = self.position.row;
         let col = self.position.col;
 
@@ -75,36 +82,36 @@ impl Guard {
                 if row == 0 {
                     None
                 } else {
-                    Some((world.grid[row - 1][col], Position::new(row - 1, col)))
+                    Some((grid[row - 1][col], Position::new(row - 1, col)))
                 }
             }
             Direction::Down => {
-                if row == world.grid.len() - 1 {
+                if row == grid.len() - 1 {
                     None
                 } else {
-                    Some((world.grid[row + 1][col], Position::new(row + 1, col)))
+                    Some((grid[row + 1][col], Position::new(row + 1, col)))
                 }
             }
             Direction::Left => {
                 if col == 0 {
                     None
                 } else {
-                    Some((world.grid[row][col - 1], Position::new(row, col - 1)))
+                    Some((grid[row][col - 1], Position::new(row, col - 1)))
                 }
             }
             Direction::Right => {
-                if col == world.grid[0].len() - 1 {
+                if col == grid[0].len() - 1 {
                     None
                 } else {
-                    Some((world.grid[row][col + 1], Position::new(row, col + 1)))
+                    Some((grid[row][col + 1], Position::new(row, col + 1)))
                 }
             }
         }
     }
 
     /// Returns the next guard position, otherwise None if the guard is looking at the edge of the world.
-    fn next(&self, world: &World) -> Option<Self> {
-        let peek = self.peek(world);
+    fn next(&self, grid: &[Vec<char>]) -> Option<Self> {
+        let peek = self.peek(grid);
         match peek {
             Some(('#', _)) => {
                 let direction = self.direction.turn_right();
@@ -116,45 +123,99 @@ impl Guard {
     }
 }
 
+#[derive(Debug, Clone)]
 struct World {
     grid: Vec<Vec<char>>,
-    guard: Guard,
 }
 
 impl World {
     fn new(input: &str) -> Self {
         let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-        // find guard
-        for (row, line) in grid.iter().enumerate() {
+        Self { grid }
+    }
+
+    fn find_guard(&self) -> Guard {
+        for (row, line) in self.grid.iter().enumerate() {
             for (col, &cell) in line.iter().enumerate() {
                 if let Ok(direction) = Direction::try_from(cell) {
                     let position = Position::new(row, col);
-                    let guard = Guard::new(position, direction);
-                    return Self { grid, guard };
+                    return Guard::new(position, direction);
                 }
             }
         }
         panic!("guard not found");
     }
 
-    fn run(&mut self) -> usize {
-        let mut path = vec![self.guard.position];
-        loop {
-            let next = self.guard.next(self);
-            match next {
-                Some(guard) => {
-                    self.guard = guard;
-                    path.push(guard.position);
-                }
-                None => break,
-            }
-        }
-        path.into_iter().collect::<HashSet<Position>>().len()
+    fn with_distraction(&self, distraction: Position) -> Self {
+        let mut world = self.clone();
+        world.grid[distraction.row][distraction.col] = '#';
+        world
     }
 }
 
-pub fn part_2(_input: &str) -> Option<i32> {
-    None
+fn run(world: &World, guard: Guard) -> usize {
+    let mut path = HashSet::new();
+    path.insert(guard.position);
+    let mut guard = guard;
+    loop {
+        let next = guard.next(&world.grid);
+        match next {
+            Some(next_guard) => {
+                guard = next_guard;
+                path.insert(guard.position);
+            }
+            None => break,
+        }
+    }
+    path.len()
+}
+
+fn run_with_simulation(world: &World, mut guard: Guard) -> usize {
+    let mut visited = HashSet::new();
+    visited.insert(guard);
+
+    let mut successful_distractions = 0;
+
+    loop {
+        match guard.peek(&world.grid) {
+            Some(('#', _)) => {}
+            None => {}
+            Some((_, position)) => {
+                let distracted = world.with_distraction(position);
+                let mut visited = visited.clone();
+                if loops(&distracted, guard, &mut visited) {
+                    successful_distractions += 1;
+                }
+            }
+        }
+
+        guard = match guard.next(&world.grid) {
+            Some(guard) => guard,
+            None => break,
+        };
+    }
+
+    successful_distractions
+}
+
+fn loops(world: &World, guard: Guard, visited: &mut HashSet<Guard>) -> bool {
+    let next = guard.next(&world.grid);
+    match next {
+        Some(next) => {
+            if visited.contains(&next) {
+                return true;
+            }
+            visited.insert(next);
+            loops(world, next, visited)
+        }
+        None => false,
+    }
+}
+
+pub fn part_2(input: &str) -> Option<i32> {
+    let world = World::new(input);
+    let guard = world.find_guard();
+    Some(run_with_simulation(&world, guard) as i32)
 }
 
 #[cfg(test)]
