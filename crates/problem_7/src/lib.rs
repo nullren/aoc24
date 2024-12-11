43a67184f03af9b133b09f94042d64c4474424d8
Renning Bruns <ren@renmail.net>
2024-12-10 20:34:40 -0800

    start a test

--- crates/problem_7/src/lib.rs
+++ crates/problem_7/src/lib.rs
@@ -10,12 +10,20 @@ pub fn part_2(_input: &str) -> Option<i32> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
 
     #[test]
     fn test_part_1() {
         let result = part_1(INPUT);
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3749));
     }
 
     #[test]
