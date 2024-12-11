8bd86379502c3d860814d4aad50fe50c7f51507a
Renning Bruns <ren@renmail.net>
2024-12-10 21:24:43 -0800

    prob7p1 solved

--- crates/problem_7/src/lib.rs
+++ crates/problem_7/src/lib.rs
@@ -1,5 +1,40 @@
-pub fn part_1(_input: &str) -> Option<i32> {
-    None
+mod sequence;
+
+pub fn part_1(input: &str) -> Option<i64> {
+    let mut total = 0;
+    for line in input.lines() {
+        let parts = line.split_ascii_whitespace().collect::<Vec<_>>();
+        let depth = parts[0].strip_suffix(":").unwrap().parse::<i64>().unwrap();
+        let nums = parts[1..]
+            .iter()
+            .map(|&s| s.parse::<i64>().unwrap())
+            .collect::<Vec<_>>();
+        if has_valid_expression(&nums, depth) {
+            total += depth;
+        }
+    }
+    Some(total)
+}
+
+#[derive(Clone, Copy)]
+enum Ops {
+    Add,
+    Mul,
+}
+
+fn has_valid_expression(nums: &[i64], total: i64) -> bool {
+    let elems = [Ops::Add, Ops::Mul];
+    let n = nums.len();
+    sequence::SequenceIterator::new(&elems, n - 1).any(|ops| {
+        let mut result = nums[0];
+        for (i, &op) in ops.iter().enumerate() {
+            match op {
+                Ops::Add => result += nums[i + 1],
+                Ops::Mul => result *= nums[i + 1],
+            }
+        }
+        result == total
+    })
 }
 
 pub fn part_2(_input: &str) -> Option<i32> {
