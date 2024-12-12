0b1a978b3ae7e1d9b2a2d6006980fccd3dcaea82
Renning Bruns <ren@renmail.net>
2024-12-12 07:40:42 -0800

    started day 11

--- crates/runner/src/main.rs
+++ crates/runner/src/main.rs
@@ -1,7 +1,7 @@
-use problem_10::{part_1, part_2};
+use problem_11::{part_1, part_2};
 
 fn main() {
-    let input = include_str!("../inputs/10.txt");
+    let input = include_str!("../inputs/11.txt");
     println!("part1: {:?}", part_1(input));
     println!("part2: {:?}", part_2(input));
 }
