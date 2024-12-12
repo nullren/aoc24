062ae4f0abdc3c2b5fb2ae81962efa91c8e98abb
Renning Bruns <ren@renmail.net>
2024-12-11 20:02:34 -0800

    we did part 2

--- crates/problem_9/src/lib.rs
+++ crates/problem_9/src/lib.rs
@@ -28,6 +28,37 @@ fn defrag_string(blocks: &mut [Option<u32>]) {
     }
 }
 
+fn defrag_files(blocks: &mut [Option<u32>]) {
+    let mut right = blocks.len() - 1;
+    while right > 0 {
+        if let Some(file_id) = blocks[right] {
+            let mut left = right;
+            while left > 0 && blocks[left] == blocks[right] {
+                left -= 1;
+            }
+            left += 1;
+            let file_size = right - left + 1;
+            println!(
+                "file_id: {}, left: {}, right: {}, size: {}",
+                file_id, left, right, file_size
+            );
+            // find first empty block of the same size
+            for i in 0..left {
+                if blocks[i..i + file_size].iter().all(|&b| b.is_none()) {
+                    blocks[i..i + file_size]
+                        .iter_mut()
+                        .for_each(|b| *b = Some(file_id));
+                    blocks[left..=right].iter_mut().for_each(|b| *b = None);
+                    break;
+                }
+            }
+            right = left - 1;
+        } else {
+            right -= 1;
+        }
+    }
+}
+
 fn construct_blocks(map: &[u32]) -> Vec<Option<u32>> {
     let mut blocks = Vec::new();
     let mut file_id = 0;
@@ -58,8 +89,12 @@ fn checksum(blocks: &[Option<u32>]) -> u64 {
     sum
 }
 
-pub fn part_2(_input: &str) -> Option<i32> {
-    None
+pub fn part_2(input: &str) -> Option<u64> {
+    let nums = parse_digits(input);
+    let mut blocks = construct_blocks(&nums);
+    defrag_files(&mut blocks);
+    let result = checksum(&blocks);
+    Some(result as u64)
 }
 
 #[cfg(test)]
@@ -73,7 +108,7 @@ mod tests {
         }
     }
 
-    fn blocks_as_string(blocks: Vec<Option<u32>>) -> String {
+    fn blocks_as_string(blocks: &[Option<u32>]) -> String {
         blocks.iter().map(|b| block_as_string(*b)).collect()
     }
 
@@ -84,7 +119,7 @@ mod tests {
         let nums = parse_digits(INPUT);
         let result = construct_blocks(&nums);
         let expected = "00...111...2...333.44.5555.6666.777.888899";
-        assert_eq!(blocks_as_string(result), expected.to_string());
+        assert_eq!(blocks_as_string(&result), expected.to_string());
     }
 
     #[test]
@@ -95,7 +130,13 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        let result = part_2(INPUT);
-        assert_eq!(result, None);
+        // let result = part_2(INPUT);
+        let nums = parse_digits(INPUT);
+        let mut blocks = construct_blocks(&nums);
+        println!("{:?}", blocks_as_string(&blocks));
+        defrag_files(&mut blocks);
+        println!("{:?}", blocks_as_string(&blocks));
+        let result = checksum(&blocks);
+        assert_eq!(Some(result), Some(2858));
     }
 }
