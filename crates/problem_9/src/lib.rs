56a891abca985529d0ba2fad8b28e2f8280a67ad
Renning Bruns <ren@renmail.net>
2024-12-11 17:11:34 -0800

    test pases

--- crates/problem_9/src/lib.rs
+++ crates/problem_9/src/lib.rs
@@ -1,5 +1,22 @@
-pub fn part_1(_input: &str) -> Option<i32> {
-    None
+pub fn part_1(input: &str) -> Option<i32> {
+    let nums = parse_input(input);
+    let result = checksum(&nums);
+    Some(result as i32)
+}
+
+fn parse_input(input: &str) -> Vec<u8> {
+    input
+        .chars()
+        .filter_map(|c| c.to_digit(10).map(|d| d as u8))
+        .collect()
+}
+
+fn checksum(nums: &[u8]) -> u64 {
+    let mut sum = 0;
+    for (i, &num) in nums.iter().enumerate() {
+        sum += i as u64 * num as u64;
+    }
+    sum
 }
 
 pub fn part_2(_input: &str) -> Option<i32> {
@@ -10,12 +27,12 @@ pub fn part_2(_input: &str) -> Option<i32> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "0099811188827773336446555566";
 
     #[test]
     fn test_part_1() {
         let result = part_1(INPUT);
-        assert_eq!(result, None);
+        assert_eq!(result, Some(1928));
     }
 
     #[test]
