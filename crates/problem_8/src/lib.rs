ecf87dd8d135c1954f0aa6b9d5b6b0b7a61d6fe3
Renning Bruns <ren@renmail.net>
2024-12-11 16:54:23 -0800

    p8p2 done!

--- crates/problem_8/src/lib.rs
+++ crates/problem_8/src/lib.rs
@@ -58,8 +58,52 @@ pub fn part_1(input: &str) -> Option<i32> {
     Some(antinodes.len() as i32)
 }
 
-pub fn part_2(_input: &str) -> Option<i32> {
-    None
+pub fn part_2(input: &str) -> Option<i32> {
+    let mut cols = 0;
+    let mut rows = 0;
+    let mut char_locs = HashMap::new();
+    for (row, line) in input.lines().enumerate() {
+        for (col, c) in line.chars().enumerate() {
+            if row > rows {
+                rows = row;
+            }
+            if col > cols {
+                cols = col;
+            }
+            if c != '.' {
+                char_locs
+                    .entry(c)
+                    .or_insert_with(Vec::new)
+                    .push(Position::new(row as i64, col as i64));
+            }
+        }
+    }
+    // println!("map dimensions: {} {}", rows, cols);
+    // println!("character locations: {:?}", char_locs);
+
+    let mut antinodes = HashSet::new();
+    for locs in char_locs.values() {
+        for i in 0..locs.len() {
+            for j in 0..locs.len() {
+                if i == j {
+                    continue;
+                }
+                for k in -50..=50 {
+                    let antinode = locs[j] + (locs[j] - locs[i]).scale(k);
+                    if antinode.row >= 0
+                        && antinode.row <= rows as i64
+                        && antinode.col >= 0
+                        && antinode.col <= cols as i64
+                    {
+                        antinodes.insert(antinode);
+                    }
+                }
+            }
+        }
+    }
+    // println!("antinodes: {:?}", antinodes);
+
+    Some(antinodes.len() as i32)
 }
 
 #[cfg(test)]
@@ -88,6 +132,6 @@ mod tests {
     #[test]
     fn test_part_2() {
         let result = part_2(INPUT);
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34));
     }
 }
