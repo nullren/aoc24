44c0c706f4f9fd721e714f5402b394b0ebe2908e
Renning Bruns <ren@renmail.net>
2024-12-12 07:49:53 -0800

    start something but need to change computers

--- crates/problem_11/src/lib.rs
+++ crates/problem_11/src/lib.rs
@@ -1,5 +1,15 @@
-pub fn part_1(_input: &str) -> Option<i32> {
-    None
+pub fn part_1(input: &str) -> Option<i32> {
+    input
+        .split_ascii_whitespace()
+        .map(|n| n.parse::<u32>().unwrap())
+        .map(|word| {
+            if word == 0 {
+                return vec![1];
+            }
+            if word.len() % 2 == 0 {
+                // split word in half, remove leading 0s
+            }
+        })
 }
 
 pub fn part_2(_input: &str) -> Option<i32> {
@@ -10,12 +20,12 @@ pub fn part_2(_input: &str) -> Option<i32> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "125 17";
 
     #[test]
     fn test_part_1() {
         let result = part_1(INPUT);
-        assert_eq!(result, None);
+        assert_eq!(result, Some(55312));
     }
 
     #[test]
